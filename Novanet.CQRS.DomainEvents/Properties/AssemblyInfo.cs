8095e3daeded3fa914e6e4782350a35d7d169e88
Yngve Bakken Nilsen <yngve.nilsen@gmail.com>
2013-04-02 13:22:15 +0200

    Proper Nuget packaging

--- Novanet.CQRS.DomainEvents/Properties/AssemblyInfo.cs
+++ Novanet.CQRS.DomainEvents/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DomainEvents")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Novanet.CQRS.DomainEvents")]
+[assembly: AssemblyDescription("DomainEvents for Novanet.CQRS")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DomainEvents")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCompany("Novanet AS")]
+[assembly: AssemblyProduct("Novanet.CQRS.DomainEvents")]
+[assembly: AssemblyCopyright("Copyright © 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
